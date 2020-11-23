1cf3a09e0b81057cd7c5b6f431c72d0228f6f583
miglio000 <gabrielemiglio23@gmail.com>
2020-11-23 15:25:14 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConvertToUnicode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String unicodeString =
+                "This Unicode has a 2 characters outside the " +
+                "ASCII range: \n" +
+                "Pi (\u03A0), and Sigma (\u03A3).";
+            Console.WriteLine("Original string.");
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine(unicodeString);
+            Console.WriteLine();
+            
+        }
+    }
+}
