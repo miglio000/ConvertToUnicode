ec0f1af4200cf54a0b9ab92f2f2bf5737e3e6c03
miglio000 <gabrielemiglio23@gmail.com>
2020-11-23 15:43:00 +0100

    Translate to Unicode

--- Program.cs
+++ Program.cs
@@ -6,11 +6,9 @@ namespace ConvertToUnicode
     {
         static void Main(string[] args)
         {
-            String unicodeString =
-                "This Unicode has a 2 characters outside the " +
-                "ASCII range: \n" +
-                "Pi (\u03A0), and Sigma (\u03A3).";
-            Console.WriteLine("Original string.");
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Insert a code to get the symbol: ");
+            string unicodeString = Console.ReadLine();
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine(unicodeString);
             Console.WriteLine();
